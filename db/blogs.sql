33bf5b921f49256cbe78dc8967ecc09a4840c381
mooosu <mooosu@hotmail.com>
2012-12-19 14:12:37 +0800

    add blogs list;

---
+++ db/blogs.sql
@@ -0,0 +1,15 @@
+
+SET NAMES utf8;
+-- ----------------------------
+--  Table structure for `blogs`
+-- ----------------------------
+DROP TABLE IF EXISTS `blogs`;
+CREATE TABLE `blogs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `comment` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
